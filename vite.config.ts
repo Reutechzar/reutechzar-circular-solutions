31b849fe5c6a5026a8946915026194516948d9f6
Murilo <oxelot17898@gmail.com>
2025-12-03 20:29:26 -0300

    alterando pacotes para hospedagem via pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // ← TROCA AQUI (remove o SWC)
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: "/reutechzar-circular-solutions/",
+    server: {
+        host: "::",
+        port: 8080,
     },
-  },
+    plugins: [
+        react(),
+        mode === "development" && componentTagger()
+    ].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
