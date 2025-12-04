278f7365a52f6cfa29f683b56c9b923d04f67ef7
Reutechzar <sergiogames194@gmail.com>
2025-12-03 22:35:42 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: "./reutechzar-circular-solutions/",
+    base: "/reutechzar-circular-solutions/",
     server: {
         host: "::",
         port: 8080,
